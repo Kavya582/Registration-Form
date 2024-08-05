d1e57e17b5f4998317e24d99a1db5f8fea2fb082
Kavya582 <kavyalamba963@gmail.com>
2024-08-05 22:07:43 +0530

    first commit

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ let mobile = document.getElementById('mobile');
 let date = document.getElementById('dob');
 
 let para = document.getElementById('para');
-para.style.color="black";
 let result;
-let variable;
-let i;
+
+
 
 let button = document.getElementById('button');
 button.addEventListener('click', myfun);
@@ -19,21 +18,38 @@ passwordshow.addEventListener('click',myfun2);
 
 
 
-
-
 function myfun(){
+    var letters = /^[a-zA-Z]*$/;
+    if(fname.value=="" && lname.value==""){
+        alert("Please Fill all the Required Credentials");
+    }
+    else{
+        if(!fname.value.match(letters) || !lname.value.match(letters)){
+            result = "First name and Last name  should be of alphabets";
+            para.innerHTML = result;
+            para.style.fontSize = "20px";
 
+        }
+        if(mobile.value.length!=1 && mobile.value.length!=10){
+            result = "Mobile Number Should not Exceed more than 10 Digits";
+            para.innerHTML = result;
+            para.style.fontSize = "20px";
+
+        }
+        if(fname.value.match(letters) && lname.value.match(letters)){
+          if(mobile.value.length==10){
+            result = "Registration Successful";
+            para.innerHTML = result;
+            para.style.fontSize = "20px";
+          }
+        }
+    }
    
-
-   if((fname.type=="text" && lname.type=="text" && mobile.value.length==10 && mobile.value.length!=0)){
-    alert("Your Registration is Successful");
-   }
-
-   else{
-    alert("You Must Fill The All Required Feilds");
-   }
-     
 }
+  
+    
+     
+
 
 
 function myfun2(){
